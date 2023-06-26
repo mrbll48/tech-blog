8ef84a8280b840ed67d7bcf6cacb93acfb968ba0
mrbll48 <william.marks87@gmail.com>
2023-06-26 15:16:02 -0700

    added sessions to sever.js

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 const routes = require("./controllers");
-
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "Super secret secret",
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
